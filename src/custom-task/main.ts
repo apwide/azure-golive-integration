384260dbdd0dcb582005ae7bec388cf3dadaa11b
Guillaume <guillaume@apwide.com>
2023-05-26 08:20:57 +0200

    Initial apwide commit

--- src/custom-task/main.ts
+++ src/custom-task/main.ts
@@ -1,3 +1,14 @@
-import * as TaskLibrary from "azure-pipelines-task-lib/task";
+import tl = require("azure-pipelines-task-lib/task");
 
-TaskLibrary.debug("Hello world!");
+async function run() {
+  try {
+    const environmentId: string | undefined = tl.getInput("environmentId", true);
+    console.log("Golive environmentId", environmentId);
+  }
+  catch (err) {
+    // @ts-ignore
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  }
+}
+
+run();
--- webpack.config.js
+++ webpack.config.js
@@ -8,17 +8,17 @@ const Target = WebpackCommon.GetTargetPath();
 const Settings = {
     "production": {
         Tag: "",
-        TaskGuid: "{{guid_production}}",
+        TaskGuid: "c5ed4c98-e170-4820-877d-cf58c5ee2fcd",
     },
     "development": {
         Tag: "Dev",
-        TaskGuid: "{{guid_development}}",
+        TaskGuid: "282ea6d5-b7b2-49eb-897a-a68bf88c4910",
     }
     // Can add more flavors here as needed. For example, a flavor for pre-production
 };
 
-module.exports = env => {
 
+module.exports = env => {
     const validEnvs = Object.keys(Settings);
     if (!validEnvs.includes(env)) {
         console.error(`BUILD_ENV not set correctly. Allowed values are: ${validEnvs.join(", ")}`);
@@ -48,7 +48,7 @@ module.exports = env => {
                     to: path.join(Target, "custom-task")
                 },
                 {
-                    from: path.join(__dirname, "./images/icon.png"),
+                    from: path.join(__dirname, "./images/task.png"),
                     to: path.join(Target, "custom-task", "icon.png")
                 },
                 {
@@ -60,7 +60,11 @@ module.exports = env => {
                     to: Target
                 },
                 {
-                    from: path.join(__dirname, "./images/icon.png"),
+                    from: path.join(__dirname, "./images/golive.png"),
+                    to: Target
+                },
+                {
+                    from: path.join(__dirname, "./images/task.png"),
                     to: Target
                 },
                 {
@@ -87,7 +91,7 @@ module.exports = env => {
                         "base.json"
                     ],
                     rules: [
-                        // This replacement is required to allow azure-pipelines-task-lib to load the 
+                        // This replacement is required to allow azure-pipelines-task-lib to load the
                         // json resource file correctly
                         {
                             search: /__webpack_require__\(.*\)\(resourceFile\)/,
