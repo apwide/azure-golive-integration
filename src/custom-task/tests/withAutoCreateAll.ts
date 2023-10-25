06954d8ffd120840e2e61f0cb46da9f15653648b
julien <julien@apwide.com>
2023-10-25 14:16:05 +0200

    chore(code): added prettier

--- src/custom-task/tests/withAutoCreateAll.ts
+++ src/custom-task/tests/withAutoCreateAll.ts
@@ -1,33 +1,33 @@
-import tmrm = require("azure-pipelines-task-lib/mock-run");
-import path = require("path");
-import {MockRequest} from "./utils/MockRequest";
+import tmrm = require('azure-pipelines-task-lib/mock-run')
+import path = require('path')
+import { MockRequest } from './utils/MockRequest'
 
-const taskPath = path.join(__dirname, "..", "main.js");
-const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+const taskPath = path.join(__dirname, '..', 'main.js')
+const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath)
 
-tmr.setInput("serviceConnection", "ID1");
-tmr.setInput("targetEnvironmentName", "new app new cat");
-tmr.setInput("targetApplicationName", "app");
-tmr.setInput("targetCategoryName", "cat");
-tmr.setInput("targetAutoCreate", "true");
+tmr.setInput('serviceConnection', 'ID1')
+tmr.setInput('targetEnvironmentName', 'new app new cat')
+tmr.setInput('targetApplicationName', 'app')
+tmr.setInput('targetCategoryName', 'cat')
+tmr.setInput('targetAutoCreate', 'true')
 
-const mockedRequest = new MockRequest();
-mockedRequest.foundEnvironments = [];
-mockedRequest.foundApplications = [];
-mockedRequest.createdApplication = {name: "app", id: "12"};
-mockedRequest.foundCategories = [];
-mockedRequest.createdCategory = {name: "cat", id: "13"};
+const mockedRequest = new MockRequest()
+mockedRequest.foundEnvironments = []
+mockedRequest.foundApplications = []
+mockedRequest.createdApplication = { name: 'app', id: '12' }
+mockedRequest.foundCategories = []
+mockedRequest.createdCategory = { name: 'cat', id: '13' }
 mockedRequest.createdEnvironment = {
   id: 333,
-  name: "new app new cat",
+  name: 'new app new cat',
   application: {
     id: 12
   },
   category: {
     id: 13
   }
-};
+}
 
-tmr.registerMock("request-promise-native", mockedRequest);
+tmr.registerMock('request-promise-native', mockedRequest)
 
-tmr.run();
+tmr.run()
--- src/custom-task/tests/withDeployedVersion.ts
+++ src/custom-task/tests/withDeployedVersion.ts
@@ -1,19 +1,19 @@
-import tmrm = require("azure-pipelines-task-lib/mock-run");
-import path = require("path");
-import {MockRequest} from "./utils/MockRequest";
+import tmrm = require('azure-pipelines-task-lib/mock-run')
+import path = require('path')
+import { MockRequest } from './utils/MockRequest'
 
-const taskPath = path.join(__dirname, "..", "main.js");
-const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+const taskPath = path.join(__dirname, '..', 'main.js')
+const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath)
 
-tmr.setInput("serviceConnection", "ID1");
-tmr.setInput("targetEnvironmentId", "111");
-tmr.setInput("deploymentVersionName", "ECOM 1.2.3.4");
+tmr.setInput('serviceConnection', 'ID1')
+tmr.setInput('targetEnvironmentId', '111')
+tmr.setInput('deploymentVersionName', 'ECOM 1.2.3.4')
 
-const mockedRequest = new MockRequest();
+const mockedRequest = new MockRequest()
 mockedRequest.put = async () => {
-  return {message: "Deployed version updated to ECOM 1.2.3.4"};
-};
+  return { message: 'Deployed version updated to ECOM 1.2.3.4' }
+}
 
-tmr.registerMock("request-promise-native", mockedRequest);
+tmr.registerMock('request-promise-native', mockedRequest)
 
-tmr.run();
+tmr.run()
