d171fd5966c4387776bbf0f2876c09c181cf38fa
julien <julien@apwide.com>
2023-10-30 10:51:10 +0100

    chore(dep): replaced deprecated request-promise-native by node-fetch

--- src/custom-task/tests/withAll.ts
+++ src/custom-task/tests/withAll.ts
@@ -1,7 +1,7 @@
 import tmrm = require('azure-pipelines-task-lib/mock-run')
 import path = require('path')
-import { MockRequest } from './utils/MockRequest'
 import NullAzureClient from './utils/MockAzureClient'
+import mockFetch = require('./utils/mockFetch')
 
 const taskPath = path.join(__dirname, '..', 'main.js')
 const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath)
@@ -18,27 +18,29 @@ tmr.setInput('deploymentVersionName', 'ECOM 1.2.3.4')
 tmr.setInput('environmentStatusId', '23')
 tmr.setInput('environmentUrl', 'https://my-new-url.com')
 
-const mockedRequest = new MockRequest()
-mockedRequest.foundEnvironments = []
-mockedRequest.foundApplications = []
-mockedRequest.createdApplication = { name: 'app', id: '12' }
-mockedRequest.foundCategories = []
-mockedRequest.createdCategory = { name: 'cat', id: '13' }
-mockedRequest.createdEnvironment = {
-  id: 333,
-  name: 'new app new cat',
-  application: {
-    id: 12
-  },
-  category: {
-    id: 13
-  }
-}
-mockedRequest.updatedDeployment = { message: 'Deployed version updated to ECOM 1.2.3.4' }
-mockedRequest.updatedEnvironmentStatus = { status: { id: 23 } }
-mockedRequest.updatedEnvironment = { id: 333, url: 'https://my-new-url.com' }
-
 tmr.registerMock('./AzureClient', NullAzureClient)
-tmr.registerMock('request-promise-native', mockedRequest)
+tmr.registerMock(
+  'node-fetch',
+  mockFetch({
+    foundEnvironments: [],
+    foundApplications: [],
+    foundCategories: [],
+    createdApplication: { name: 'app', id: '12' },
+    createdCategory: { name: 'cat', id: '13' },
+    createdEnvironment: {
+      id: 333,
+      name: 'new app new cat',
+      application: {
+        id: 12
+      },
+      category: {
+        id: 13
+      }
+    },
+    updatedDeployment: { message: 'Deployed version updated to ECOM 1.2.3.4' },
+    updatedEnvironmentStatus: { status: { id: 23 } },
+    updatedEnvironment: { id: 333, url: 'https://my-new-url.com' }
+  })
+)
 
 tmr.run()
